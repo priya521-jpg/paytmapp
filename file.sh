00ca1ce6f6dbc903facb2f0f2047aa819cad9d5b
Ubuntu <ubuntu@ip-172-31-0-49.ap-south-1.compute.internal>
2024-11-12 19:29:43 +0530

    commit 1

---
+++ file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir lucky
+cd lucky
+touch lckyf
+echo "look smart " >> lckyf
+
+i
+2
+3
+t
+trkjhtr
+okhgtiujgtr
+gmtr
+grtogj
+dfref
+b
+grg
+grt
+gtr
+htovtm,ekterg
+ggktr
+gtglkregmr
+grtgmg,r
+btg[lb
+glrtmg
+glrtmg
